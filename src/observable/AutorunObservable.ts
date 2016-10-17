237fb57dfb2ad5667dad1f64a78debeebcd320fa
Joatin Granlund <jg@fraktalconsulting.com>
2016-10-17 23:45:34 +0200

    Removed cursor support

--- src/observable/AutorunObservable.ts
+++ src/observable/AutorunObservable.ts
@@ -2,32 +2,31 @@ import {Observable, Subscriber} from 'rxjs';
 import {TeardownLogic} from 'rxjs/Subscription';
 import {NgZone} from "@angular/core";
 
+/**
+ * An observable that takes an autorun callback as parameter. The returned result from the callback is then passed on in
+ * the observable.
+ *
+ * @version 1.0.0
+ */
 export class AutorunObservable<T> extends Observable<T>{
-    public static create<T>(autorunCallback: () => T | Mongo.Cursor<any>): Observable<T>{
+    public static create<T>(autorunCallback: () => T): Observable<T>{
         return new AutorunObservable<T>(autorunCallback);
     }
 
     private zone: NgZone = new NgZone({});
 
     public constructor(
-        private autorunCallback: () => T | Mongo.Cursor<any>
+        private autorunCallback: () => T
     ){
         super();
     }
 
     protected _subscribe(subscriber: Subscriber<T>): TeardownLogic{
         let handle = Tracker.autorun(() => {
-            let value = this.autorunCallback();
-            if(value instanceof Mongo.Cursor){
-                let result: T = <T><Object>(<Mongo.Cursor<any>>value).fetch();
-                this.zone.run(() => {
-                    subscriber.next(result);
-                });
-            } else {
-                this.zone.run(() => {
-                    subscriber.next(<T>value);
-                });
-            }
+            let result = this.autorunCallback();
+            this.zone.run(() => {
+                subscriber.next(result);
+            });
         });
 
         return () => {
